62a5fac85bc49e16666f8bc8e0d89d481c240cd8
saharudra <rudra.saha@asu.edu>
2020-10-11 10:33:13 -0400

    Add box features sampling from an isotropic gaussian

--- dataloaders/sample_image_features.py
+++ dataloaders/sample_image_features.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 from torch_geometric.data import DataLoader
 
 import numpy as np
+import numpy_indexed as npi
 from .scitsr import ScitsrDataset
 from misc.args import scitsr_params, img_model_params
 from models.img_models import ConvBaseGFTE
@@ -13,9 +14,9 @@ dataset_params = scitsr_params()
 print(dataset_params)
 
 train_dataset = ScitsrDataset(dataset_params)
-train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False)
 
-def sample_box_features(cnnout, nodenum, pos, cell_wh, img):
+def sample_box_features(cnnout, nodenum, pos, cell_wh, img, num_samples=5, div=16.0):
     """
     cnnout:  batch_size x cnn_out_features x cnn_out_h x cnn_out_w
              Output features from image processing stem prior to sending to GCNN or MLP.
@@ -36,6 +37,7 @@ def sample_box_features(cnnout, nodenum, pos, cell_wh, img):
     for i in range(nodenum.size()[0]):
         imgpos=pos[cnt:cnt+nodenum[i], :]
         cellpos = cell_wh[cnt:cnt+nodenum[i], :]
+        
         # Create sampling grid using gaussian sampling around cell text center 
         # Use cell text height and width to define the covariance matrix for sampling
         sampling_grid = np.empty((0, 2))
@@ -43,39 +45,49 @@ def sample_box_features(cnnout, nodenum, pos, cell_wh, img):
         cellpos = cellpos.numpy()
         for idx, cen_pos in enumerate(imgpos):
             min_cellpos = min(cellpos[idx])
-            cov = [[min_cellpos/16.0, min_cellpos/16.0], [min_cellpos/16.0, min_cellpos/16.0]]
-            samples = np.random.multivariate_normal(cen_pos, cov, 5)
+            # Sample from an isotropic gaussian with mean as cell text centroid 
+            cov = [[min_cellpos/div, 0], [0, min_cellpos/div]]
+            samples = np.random.multivariate_normal(cen_pos, cov, num_samples)
             sampling_grid = np.append(sampling_grid, samples, axis = 0)
 
         # Clip sampling grid to keep values between -1 and 1
         sampling_grid = np.clip(sampling_grid, -1.0, 1.0)
-        # Sanity check: Scatter plot sampling_grid, imgpos
-        x_imgpos_scatter = imgpos[:, 1]
-        y_imgpos_scatter = imgpos[:, 0]
-        x_sampling_grid = sampling_grid[:, 1]
-        y_sampling_grid = sampling_grid[:, 0]
-        img = img.squeeze(0).permute(1, 2, 0)
-        img = img.numpy()
-        plt.subplot(221)
-        plt.imshow(img)
-        plt.subplot(222)
-        plt.scatter(x_imgpos_scatter, y_imgpos_scatter, c='red')
-        plt.scatter(x_sampling_grid, y_sampling_grid, c='green')
-        plt.show()
-        import pdb; pdb.set_trace()
 
-        # imgpos = imgpos.unsqueeze(0) 
-        # imgpos = imgpos.unsqueeze(0)
-        # cnnin = cnnout[i].unsqueeze(0)  # Single graph
-        # sout = F.grid_sample(cnnin, imgpos, mode='bilinear', padding_mode='border')
-        # cnt+=nodenum[i]
-        # sout=sout.squeeze(0)
-        # sout=sout.squeeze(1)
-        # sout = sout.permute(1, 0) # num_box*feature_num
-        # if i==0:
-        #     out = sout
-        # else:
-        #     out = torch.cat((out,sout),0)
+        # Sanity check: Scatter plot sampling_grid, imgpos
+        # x_imgpos_scatter = imgpos[:, 1]
+        # y_imgpos_scatter = imgpos[:, 0]
+        # x_sampling_grid = sampling_grid[:, 1]
+        # y_sampling_grid = sampling_grid[:, 0]
+        # img = img[0].squeeze(0).permute(1, 2, 0)
+        # img = img.numpy()
+        # plt.subplot(221)
+        # plt.imshow(img)
+        # plt.subplot(222)
+        # plt.scatter(x_imgpos_scatter, y_imgpos_scatter, c='red')
+        # plt.scatter(x_sampling_grid, y_sampling_grid, c='green')
+        # plt.show()
+        # import pdb; pdb.set_trace()
+        
+        # Conver back to torch tensor
+        imgpos = torch.FloatTensor(imgpos)
+        sampling_grid = torch.FloatTensor(sampling_grid)
+        sampling_grid = sampling_grid.unsqueeze(0) 
+        sampling_grid = sampling_grid.unsqueeze(0)
+        cnnin = cnnout[i].unsqueeze(0)  # Single graph
+        sout = F.grid_sample(cnnin, sampling_grid, mode='bilinear', padding_mode='border')
+        cnt+=nodenum[i]
+        sout=sout.squeeze(0)
+        sout=sout.squeeze(1)
+        sout = sout.permute(1, 0) # (num_box * num_samples) x feature_num
+        idx = np.repeat(np.arange(1, nodenum[i].item() + 1), num_samples)
+        sout = sout.detach().numpy()
+        sout = np.insert(sout, 0, idx, axis=1)
+        sout = npi.group_by(sout[:, 0]).split(sout[:, 1:]).reshape(nodenum[i].item(), -1)
+        sout = torch.FloatTensor(sout)
+        if i==0:
+            out = sout
+        else:
+            out = torch.cat((out,sout),0)
     return out
 
 img_params = img_model_params()
@@ -86,5 +98,5 @@ for idx, data in enumerate(train_loader):
     img, imgpos, nodenum, cell_wh = data.img, data.imgpos, data.nodenum, data.cell_wh
     print(img.shape)
     img_features = img_model(img)
-    box_features = sample_box_features(img_features, nodenum, imgpos, cell_wh, img)
+    box_features = sample_box_features(img_features, nodenum, imgpos, cell_wh, img, num_samples=10, div=32.0)
     import pdb; pdb.set_trace()
