6ddaf73834b68eb742f6d9e8f3c0919c5f2792d5
saharudra <rudra.saha@asu.edu>
2021-01-16 17:39:33 +0000

    temp

--- dataloaders/plot_pubtabnet.py
+++ dataloaders/plot_pubtabnet.py
@@ -3,11 +3,21 @@ Visualizing PubTabNet dataset's ground truth.
 """
 import json
 import cv2
+import matplotlib.pyplot as plt
 
 # load json file
-root = '/datatop_1/rudra/table_recognition/datasets/pubtabnet/'
-annotation_filename = root + 'PubTabNet_2.0.0.jsonl'
+root = '/datatop_1/rudra/table_recognition/PubTabNet/examples/'
+annotation_filename = root + 'PubTabNet_Examples.jsonl'
 
-annotations = json.load(annotation_filename)
-print(annotations)
+annotations = []
+with open(annotation_filename, 'r') as jaf:
+    for line in jaf:
+        annotations.append(json.loads(line))
 
+print(len(annotations))
+# import pdb; pdb.set_trace()
+# load image
+img_filename = root + annotations[0]['filename']
+img = cv2.cvtColor(cv2.imread(img_filename), cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.show()
