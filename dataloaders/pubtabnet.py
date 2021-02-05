82722c8e2fd5d9d1acc7c9b84c760e847cabf61d
saharudra <rudra.saha@asu.edu>
2021-02-04 19:26:22 -0500

    Add pubtabnet dataloader

--- dataloaders/pubtabnet.py
+++ dataloaders/pubtabnet.py
@@ -16,7 +16,8 @@ from ops.utils import resize_image
 
 class PubTabNetDataset(Dataset):
     """
-    Dataset Generator for S-A and S-B experimentation regimies.
+    Dataset Generator for S-A and S-B experimentation regimies for transformer
+    based architecture.
     S-A: Input to the model is only the image of the table. 
          Model consists of a module to extract bounding boxes
          and cluster them into cell-text bounding boxes.
@@ -29,6 +30,7 @@ class PubTabNetDataset(Dataset):
 
     Sanity Checks:
     * Plot position features on image
+    TODO: MCTS based sampling for row and col classification for pairs of cell-texts.
     """
 
     def __init__(self, params, partition='train', transform=None, pre_transform=None):
@@ -68,12 +70,15 @@ class PubTabNetDataset(Dataset):
 
      # remove empty cells
      def remove_empty_cell(self, chunks):
+
           new_chunks = []
+
           for chunk in chunks:
                if 'bbox' not in chunk:
                     continue
                else:
                     new_chunks.append(chunk)
+
           return new_chunks
 
      def readlabel(self, idx):
@@ -89,6 +94,8 @@ class PubTabNetDataset(Dataset):
                      'end_row': 0-indexed integer value,
                      'end_col': 0-indexed integer value}, ...
                ]
+
+               Bounding boxes have been modified based on image resizing parameter.
           """
           imgfn = self.imglist[idx]
 
@@ -117,7 +124,7 @@ class PubTabNetDataset(Dataset):
                     offset = (self.params.img_size - math.floor((self.params.img_size * h) / w)) / 2
                
                elif h > w:
-                    # lll'y if height > width, offset added in width or x direction
+                    # similarly if height > width, offset added in width or x direction
                     # scale bbox in y direction directly
                     offset = (self.params.img_size - math.floor((self.params.img_size * w) / h)) / 2
                
@@ -148,7 +155,8 @@ class PubTabNetDataset(Dataset):
                               y0 = int((y0 / h) * h_n)
                               y1 = int((y1 / h) * h_n)
 
-                         cell['bbox'] = [x0, y0, x1, y1]
+                         # Changing bbox indexs to match scitsr/icdar2013/... dataloaders.
+                         cell['bbox'] = [x0, x1, y0, y1]
           
           return img, chunks
 
@@ -157,13 +165,125 @@ class PubTabNetDataset(Dataset):
           return len(self.imglist)
      
      def cal_chk_limits(self, chunks):
-          x_min = 
-               
-                    
 
+          x_min = min(chunks, key=lambda p: p['bbox'][0])['bbox'][0]
+          y_min = min(chunks, key=lambda p: p['bbox'][1])['bbox'][1]
+          x_max = max(chunks, key=lambda p: p['bbox'][2])['bbox'][2]
+          y_max = max(chunks, key=lambda p: p['bbox'][3])['bbox'][3]
+          hlist = [p['bbox'][3] - p['bbox'][1] for p in chunks]
+          avg_hei = sum(hlist) / len(hlist)
+          width = x_max - x_min + 2 * avhei
+          height = y_max - y_min + 0.5 * 2 * avhei
+          # Maintain position features index similar to scitsr/icdar2013/... dataloader
+          # for carryign out transfer-learning experiments.
 
+          return [x_min, x_max, y_min, y_max, width, height, avhei]
 
+     def pos_feature(self, chk, cl):
 
+          # Same as that of scitsr/icdar2013/... dataloaders as indexing has been changed.
+          # See methods: cal_chk_limits, readlabel
+          x1 = (chk["bbox"][0] - cl[0] + cl[6]) / cl[4]
+          x2 = (chk["bbox"][1] - cl[0] + cl[6]) / cl[4]
+          y1 = (chk["bbox"][2] - cl[2] + 0.5 * cl[6]) / cl[5]
+          y2 = (chk["bbox"][3] - cl[2] + 0.5 * cl[6]) / cl[5]
+          center_x = (x1 + x2) * 0.5  
+          center_y = (y1 + y2) * 0.5
+          width = x2 - x1  
+          height = y2 - y1  
 
-     
+          return [x1, x2, y1, y2, center_x, center_y, width, height]
+
+     def augmentation_chk(self, chunks):
+          for chk in chunks:
+               chk["bbox"][0] += random.normalvariate(0, 1)
+               chk["bbox"][1] += random.normalvariate(0, 1)
+               chk["bbox"][2] += random.normalvariate(0, 1)
+               chk["bbox"][3] += random.normalvariate(0, 1)
+
+     def cal_row_label(slef, tbpos):
+          """
+          Calculating for all-pairs of cell-texts. This is for the transformer based
+          architecture without sampling for loss calculation.
+          Here, row_classification is done for all of the cell-texts w.r.t the first cell text
+          then for remaining w.r.t second cell-text and so on..
+          After the transformer module, concatenation of cell-text being done in the same manner
+          to maintain correspondance between input and output for each table image.
+          TODO: MCTS sampling based row classification.
+          """
+          y = []
+
+          for cell_i in range(len(tbpos)):
+               for cell_j in range(cell_i + 1, len(tbpos)):
+                    source_start, source_end = tbpos[cell_i][0], tbpos[cell_i][1]
+                    target_start, target_end = tbpos[cell_j][0], tbpos[cell_j][1]
+                    if (source_start >= target_start and source_end <= target_end):
+                         y.append(1)
+                    elif (target_start >= source_start and target_end <= source_end):
+                         y.append(1)
+                    else:
+                         y.append(0)
+
+          return y
+
+     def cal_col_label(self, data, tbpos):
+          """
+          Calculating for all-pairs of cell-texts. This is for the transformer based
+          architecture without sampling for loss calculation.
+          Here, col_classification is done for all of the cell-texts w.r.t the first cell text
+          then for remaining w.r.t second cell-text and so on..
+          After the transformer module, concatenation of cell-text being done in the same manner
+          to maintain correspondance between input and output for each table image.
+          TODO: MCTS sampling based col classification.
+          """
+          y = []
+
+          for cell_i in range(len(tbpos)):
+               for cell_j in range(cell_i + 1, len(tbpos)):
+                    source_start, source_end = tbpos[cell_i][2], tbpos[cell_i][3]
+                    target_start, target_end = tbpos[cell_j][2], tbpos[cell_j][3]
+                    if (source_start >= target_start and source_end <= target_end):
+                         y.append(1)
+                    elif (target_start >= source_start and target_end <= source_end):
+                         y.append(1)
+                    else:
+                         y.append(0)
+
+          return y
+
+     def get(self, idx):
+          img, chunks = self.readlabel(idx)
+
+          if self.params.augment_chunk:
+               self.augmentation_chk(chunks)
+
+          x, pos, tbpos, imgpos, cell_wh = [], [], [], [], []
+
+          chunks = self.remove_empty_cell(chunks)
+          # Calculating chunk limits after removing empty cells
+          cl = self.cal_chk_limits(chunks)
+
+          for chunk in chunks:
+               xt = self.pos_feature(chunk, cl)
+               x.append(xt)
+               pos.append(xt[4:6])
+               tbpos.append([chunk['start_row'], chunk['end_row'], chunk['start_col'], chunk['end_col']])
+               imgpos.append([(1.0 - xt[5]) * 2 - 1.0, xt[4] * 2 - 1.0])
+               cell_wh.append([xt[-2], xt[-1]])
+
+          x = torch.FloatTensor(x)
+          pos = torch.FloatTensor(pos)
+          data = Data(x=x, pos=pos)
+          
+          y_row = self.cal_row_label(tbpos)
+          y_col = self.cal_col_label(tbpos)
+
+          data.y_row = torch.LongTensor(y_row) 
+          data.y_col = torch.LongTensor(y_col)
+          data.img = img
+          data.imgpos = torch.FloatTensor(imgpos)
+          data.cell_wh = torch.FloatTensor(cell_wh)
+          data.nodenum = torch.LongTensor([len(chunks)])
+          
+          return data
        
